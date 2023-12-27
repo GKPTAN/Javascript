a5cdea91b43600ab6483d8467926ba8674292ea9
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2023-12-27 12:26:58 -0300

    exercicio prático

---
+++ aula12/javascript/index.js
@@ -0,0 +1,8 @@
+var agora = new Date()
+var hora = agora.getHours()
+var img = document.getElementById('img')
+var p = document.getElementById('hora')
+p.innerHTML = `Agora são ${hora} horas`
+if (hora > 5 && hora < 12) {
+            
+}
