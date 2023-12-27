4cc55742f1886cca48a5c456d708162f37a06c08
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2023-12-27 17:03:01 -0300

    atualização

--- aula12/javascript/index.js
+++ aula12/javascript/index.js
@@ -1,8 +1,21 @@
-var agora = new Date()
-var hora = agora.getHours()
-var img = document.getElementById('img')
-var p = document.getElementById('hora')
-p.innerHTML = `Agora são ${hora} horas`
-if (hora > 5 && hora < 12) {
-            
+function carregar() {
+    var p = document.getElementById('hora')
+    var img = document.getElementById('foto')
+    var footer = document.querySelector('footer')
+    var titulo = document.querySelector('h1')
+    var data = new Date()
+    var hora = data.getHours()
+    p.innerHTML = `Agora são ${hora} horas`
+    if (hora > 5 && hora < 12) {
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#0863A5'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#F3B03C'
+        footer.style.color = 'black'
+        titulo.style.color = 'black'
+    } else {
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#081537'
+    }
 }
