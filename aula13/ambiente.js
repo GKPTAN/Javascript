2dfc4fd3a19334e0a3dd36ce6b2e34575b7c4823
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2023-12-29 17:59:21 -0300

    atualização

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,4 +1,14 @@
+// Estrutura de repetição com teste lógico no final
 var c = 1
-while (c < 6) {
-    console.log('Tudo bem?')
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+/* Estrutura de repetição com teste lógico no inicio
+    var c = 1
+    while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
 }
+*/
