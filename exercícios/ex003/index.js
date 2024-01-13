db3847afa6841a702f2f04fd9971d8ea43fdb39e
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2024-01-13 16:14:38 -0300

    updates

--- exercícios/ex003/index.js
+++ exercícios/ex003/index.js
@@ -1,3 +1,23 @@
-var cgd = document.getElementById('qtcigarros')
-var anosdef = document.getElementById('anosdef')
-var res = document.getElementById('res')
+function calcular() {
+let cgd = document.getElementById('qtcigarros')
+let anosdef = document.getElementById('anosdef')
+let res = document.getElementById('res')
+if (cgd.value.length === 0 || anosdef.value.length === 0) {
+window.alert('[ERRO] Preencha os dados e tente novamente')
+} else {
+    let cigarros = Number(cgd.value)
+    let anos = Number(anosdef.value)
+    let restanteVida = 10 * cigarros
+    let tempoFumante = anos * 365
+    let tempoRm = restanteVida * tempoFumante
+    let tempoHoras = tempoRm / 60
+    let tempoDias = tempoHoras / 24
+    res.innerHTML = `Você vai perder no total ${tempoDias} dias de vida`
+}
+}
+
+function limpar() {
+    document.getElementById('qtcigarros').value = ''
+    document.getElementById('anosdef').value = ''
+    res.innerHTML = ''
+}
