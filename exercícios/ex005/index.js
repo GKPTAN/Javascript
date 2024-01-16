2ea1195fc73995190fcefc95865383014dc5f8fe
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2024-01-16 17:37:30 -0300

    new exercise

---
+++ exercícios/ex005/index.js
@@ -0,0 +1,21 @@
+function calcular() {
+    var agora = new Date()
+    var anoAtual = agora.getFullYear()
+    var nasc = document.getElementById('nasc')
+    var res = document.getElementById('res')
+    if (nasc.value.length === 0) {
+        window.alert('[ERRO], preencha os dados e tente novamente')
+    } else {
+        let nascimento = Number(nasc.value)
+        let idade = anoAtual - nascimento
+        if (idade < 16) {
+            res.innerHTML = 'Não pode votar'
+        } else {
+            if (idade >= 16 && idade < 18 || idade > 60) {
+                res.innerHTML = 'Voto Opcional'
+            } else {
+                res.innerHTML = 'Voto Obrigatório'
+            }
+        }
+    }
+}
