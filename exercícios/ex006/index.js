fd53f604ed68bbe7e83fb6e594c74f65faaca93f
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2024-01-21 18:04:57 -0300

    new test

---
+++ exercícios/ex006/index.js
@@ -0,0 +1,13 @@
+function analisar() {
+    let reta1 = Number(document.getElementById('reta1').value)
+    let reta2 = Number(document.getElementById('reta2').value)
+    let reta3 = Number(document.getElementById('reta3').value)
+    let res = document.getElementById('res')
+}
+
+function requirido() {
+    reta1.reportValidity()
+    reta2.reportValidity()
+    reta3.reportValidity()
+}
+
