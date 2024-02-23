04da26027ea35d85faa6a6b8e8e66eb8216e1a58
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2024-02-23 14:51:04 -0300

    update

--- exercícios/ex010/index.js
+++ exercícios/ex010/index.js
@@ -1,3 +1,7 @@
 function contagem() {
-    
+    var res = document.getElementById('res')
+    for(var c = 6;c <= 11;c++) {
+        res.innerHTML += ` ${c},`
+    }
+    res.innerHTML += ` ACABOU!`
 }
