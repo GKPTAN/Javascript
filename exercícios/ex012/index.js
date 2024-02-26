c925c6c64e91ca947999327426db7f8e2c827ae9
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2024-02-26 15:10:46 -0300

    update

--- exercícios/ex012/index.js
+++ exercícios/ex012/index.js
@@ -1,34 +1,49 @@
+// Função para verificar
 function verificar() {
-    const secretoNum = Math.floor(Math.random() * 5) + 1; // Número aleatório de 1 a 5
-    const maxTentativas = 4;
-    const numero = document.getElementById('numero');
-    const oculto = document.getElementById('oculto');
-    const res = document.getElementById('res');
-    const gameSection = document.getElementById('game');
+    // Variáveis
+    const numero = document.getElementById('numero')
+    const res = document.getElementById('res')
+    const maxTentativas = 4
+    const numeroSorteado = sorteio(1, 16)
+    const numeroEscolhido = Number(numero.value)
 
-    for (let attempt = 1; attempt <= maxTentativas; attempt++) {
-        const userGuess = parseInt(numero.value);
+    let acertou = false
 
-        if (userGuess === secretoNum) {
-            res.innerHTML = `Você acertou na tentativa ${attempt}!`;
-            showResultSection();
-            return;
+    for (let tentativa = 1; tentativa <= maxTentativas; tentativa++) {
+        if (numeroEscolhido === numeroSorteado) {
+            res.innerHTML = 'PARABENS, VOCÊ ACERTOU!';
+            acertou = true
+            break;
         } else {
-            res.innerHTML = `Você errou na tentativa ${attempt}.`;
+            res.innerHTML = `VOCÊ ERROU! Tente novamente. Tentativa ${tentativa} de ${maxTentativas}`
+            // Adiciona um pequeno atraso antes de limpar a mensagem
+            setTimeout(() => {
+                res.innerHTML = '';
+            }, 1000)
+            numero.value = ''
+            break
         }
     }
 
-    // Se o usuário falhou em todas as tentativas
-    mostrarOculto();
-    esconderSectionGame();
+    if (!acertou && maxTentativas === 0) {
+        showHiddenSection()
+        hideGameSection()
+    }
 }
 
-function mostrarOculto() {
+// Função para exibir a seção oculta
+function showHiddenSection() {
     const oculto = document.getElementById('oculto')
-    oculto.style.display = 'flex'
+    oculto.classList.remove('hidden')
 }
 
+// Função para ocultar a seção do jogo
 function hideGameSection() {
-    const gameSection = document.getElementById('game');
-    gameSection.classList.add('hidden');
+    const gameSection = document.getElementById('game')
+    gameSection.classList.add('hidden')
+}
+
+// Função para sorteio
+function sorteio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
