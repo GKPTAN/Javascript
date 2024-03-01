65888954fe20750bcc3f3c0212676261245b7520
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2024-03-01 13:50:18 -0300

    problema resolvido
    problema de lógica que o código não conseguia obter o valor da variável numero usando a estrutura de repetição while.

--- exercícios/ex012/index.js
+++ exercícios/ex012/index.js
@@ -1,40 +1,41 @@
 // Função para verificar
-function verificar() {
+async function verificar() {
     // Variáveis
-    const numero = document.getElementById('numero')
     const res = document.getElementById('res')
     const numeroSorteado = sorteio(1, 10)
-    const numeroEscolhido = Number(numero.value)
 
     function sorteio(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min
     }
 
-    for (let tentativa = 0; tentativa <= 4;tentativa++) {
-        const valorTentativa = obterValor(tentativa)
-        if (numeroEscolhido === numeroSorteado) {
-            res.innerHTML = 'PARABÉNS, VOCÊ ACERTOU'
-        } else {
-            res.innerHTML += `VOCÊ ERROU, tentativa ${tentativa} de 4 <br>`
-            numero.value = ''
-            numero.focus()
-            erro = true
-            break
+    let tentativa = 1
+
+    async function loop() {
+
+        let mensagens = ''
+        while (tentativa <= 4) {
+            const numero = document.getElementById('numero')
+            const numeroEscolhido = Number(numero.value)
+            if (tentativa === 4) {
+                showHiddenSection()
+                hideGameSection()
+            }
+
+            if (numeroEscolhido === numeroSorteado) {
+                res.innerHTML = 'PARABÉNS, VOCÊ ACERTOU'
+            } else {
+                mensagens += `VOCÊ ERROU, tentativa ${tentativa} de 4, seu número ${numeroEscolhido} <br>`
+                res.innerHTML = mensagens
+            }
+
+            tentativa++
+
+            await new Promise(resolve => {
+                document.getElementById('botao').addEventListener('click', resolve)
+            })
         }
     }
-
-    let erro = false
-
-    if (erro)
-
-    if (tentativa === 4) {
-        showHiddenSection()
-        hideGameSection()
-    }
-}
-
-function obterValor(tentativa) {
-    return tentativa + 1
+    loop()
 }
 
 // Função para exibir a seção oculta
@@ -49,4 +50,6 @@ function showHiddenSection() {
 function hideGameSection() {
     const gameSection = document.getElementById('game')
     gameSection.classList.add('hidden')
+    const cabeca = document.getElementById('cabeca')
+    cabeca.classList.add('hidden')
 }
