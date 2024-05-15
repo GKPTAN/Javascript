20b9c06f7985f5856946f06ee3462161d896111f
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2024-05-15 12:59:15 -0300

    Update

--- exercícios/ex016/index.js
+++ exercícios/ex016/index.js
@@ -2,10 +2,9 @@ function contar() {
     const res = document.getElementById('res')
     var c = 0
     do {
-        setInterval(() => {
-            res.innerHTML += ` ${c},`
-        }, 1000);
+        res.innerHTML += ` ${c},`
         c = c + 3
     } while (c <= 30)
     console.log(res)
+    res.innerHTML += ' acabou!'
 }
